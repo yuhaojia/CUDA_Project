a790b78412d6054a78377738b0ffd5097fe01278
Chaohua Shang <chaohuashang@wirelessprv-10-192-220-218.near.illinois.edu>
2017-11-15 19:37:35 -0600

    updata current to m2

--- ece408_src/new-forward.h
+++ ece408_src/new-forward.h
@@ -23,18 +23,32 @@ void forward(mshadow::Tensor<cpu, 4, DType> &y, const mshadow::Tensor<cpu, 4, DT
 
     
     const int B = x.shape_[0];
-    // const int M = y.shape_[1];
-    // const int C = x.shape_[1];
-    // const int H = x.shape_[2];
-    // const int W = x.shape_[3];
-    // const int K = w.shape_[3];
+    const int M = y.shape_[1];
+    const int C = x.shape_[1];
+    const int H = x.shape_[2];
+    const int W = x.shape_[3];
+    const int K = k.shape_[3];
+
+    int H_out = H - K + 1;
+    int W_out = W - K + 1; 
 
     for (int b = 0; b < B; ++b) {
-        CHECK_EQ(0, 1) << "Missing an ECE408 CPU implementation!";
+        //CHECK_EQ(0, 1) << "Missing an ECE408 CPU implementation!";
 
         /* ... a bunch of nested loops later...
             y[b][m][h][w] += x[b][c][h + p][w + q] * k[m][c][p][q];
         */
+        
+        for(int m = 0;  m < M;  m++)              // for each output feature map
+            for(int h = 0; h < H_out; h++)          // for each output element
+              for(int w = 0; w < W_out; w++) {
+                y[b][m][h][w] = 0;
+                for(int c = 0;  c < C; c++)         // sum over all input feature maps
+                  for(int p = 0; p < K; p++)	       // KxK  filter
+                    for(int q = 0; q < K; q++)  
+                      y[b][m][h][w] += x[b][c][h + p][w + q] * k[m][c][p][q];
+            }
+      
     }
 
 
